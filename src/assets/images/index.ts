40f3678008c12e00dc7a9b6c47587b70181ea3f6
unknown <duyzfclong@gmail.com>
2024-05-17 16:04:12 +0700

    done design layout login

---
+++ src/assets/images/index.ts
@@ -0,0 +1,16 @@
+import logo from "./Logo.png";
+import appStoreBanned from "./AppStore-Banned.png";
+import googlePlayBanned from "./GooglePlay-Banned.png";
+import homePhone from "./home-phones.png";
+import homePhoneImage01 from "./image-home-phone.png";
+import homePhoneImage02 from "./image2-home-phone.png";
+import logoFacebook from "./facebook.png";
+export const images = {
+  logo,
+  appStoreBanned,
+  googlePlayBanned,
+  homePhone,
+  homePhoneImage01,
+  homePhoneImage02,
+  logoFacebook,
+};
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,78 @@
-const Login = () => {
-  return <>
-    <h1>Login</h1>
-  </>
-}
+import { useEffect, useState } from "react"
+import { images } from "../../assets/images"
 
+const Login = () => {
+    const [imagePhone, setImagePhone] = useState(images.homePhoneImage01)
+    const [fadeIn, setFadeIn] = useState(true)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFadeIn(false)
+            setTimeout(() => {
+                setImagePhone(prevImage => prevImage === images.homePhoneImage01 ? images.homePhoneImage02 : images.homePhoneImage01)
+                setFadeIn(true)
+            }, 100)
+        }, 3000)
+        return () => clearInterval(interval)
+    }, [])
+
+    return (
+        <>
+            <div className="flex max-w-screen-md mx-auto pt-14 gap-8">
+                <div className="lg:w-full lg:flex lg:justify-center hidden">
+                    <div className="absolute">
+                        <img src={images.homePhone} alt="Home Phone" />
+                    </div>
+                    <div className={`relative top-6 left-[49px] ${fadeIn ? 'opacity-100 transition-opacity duration-500' : 'opacity-0'}`}>
+                        <img src={imagePhone} alt="Home Phone Image" />
+                    </div>
+                </div>
+                <div className=" lg:flex lg:flex-col lg:items-center lg:justify-center lg:space-y-8 lg:w-full mx-auto w-[360px]">
+                    <div className="w-full flex flex-col items-center space-y-4 border p-6 rounded">
+                        <div>
+                            <img src={images.logo} alt="Instagram Logo" className="py-5 w-[125px]" />
+                        </div>
+                        <form className="w-full flex flex-col space-y-2">
+                            <input
+                                type="text"
+                                placeholder="Số điện thoại, tên người dùng hoặc email"
+                                className="p-2 border border-gray-300 rounded"
+                            />
+                            <input
+                                type="password"
+                                placeholder="Mật khẩu"
+                                className="p-2 border border-gray-300 rounded"
+                            />
+                            <button className="p-2 bg-blue-400 text-white rounded">Đăng nhập</button>
+                        </form>
+                        <div className="w-full flex items-center my-4">
+                            <div className="flex-grow border-t border-gray-300"></div>
+                            <span className="mx-2 text-gray-500">OR</span>
+                            <div className="flex-grow border-t border-gray-300"></div>
+                        </div>
+                        <div className="flex items-center space-x-2">
+                            <img className="w-5" src={images.logoFacebook} alt="" />
+                            <a href="#" className="text-blue-600">Đăng nhập bằng Facebook</a>
+                        </div>
+                        <a href="#" className="text-sm">Quên mật khẩu?</a>
+                    </div>
+                    <div className="border border-gray-300 p-4 rounded text-center w-full">
+                        Bạn chưa có tài khoản ư? <a className="px-1 text-blue-500 text-lg" href="">Đăng ký</a>
+                    </div>
+                    <div className="w-full flex flex-col items-center space-y-2 mt-4">
+                        <div className="text-sm pb-5">Tải ứng dụng</div>
+                        <div className="flex space-x-2">
+                            <div className="w-32 h-10 bg-gray-300 flex items-center justify-center">
+                                <img src={images.googlePlayBanned} alt="Google Play" className="h-full" />
+                            </div>
+                            <div className="w-32 h-10 bg-gray-300 flex items-center justify-center">
+                                <img src={images.appStoreBanned} alt="Microsoft Store" className="h-full" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
 export default Login
